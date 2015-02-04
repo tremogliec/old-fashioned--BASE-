c82f2f98689631daccc93af32341c7be81e821e8
tremogliec <tremogliec@gmail.com>
2015-02-03 19:31:28 -0500

    Build 1.0
    HTML5 template for projects utilizing a Burbon's mixins for SCSS and
    layouts by Dan Kling.  Initializer, Nicolasgal Lagher's helper classes
    and a version of Foldy960 from Paravel are also used.

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php include 'includes/header.php';?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.1.0.min.js"></script>
+        <![endif]-->
+
+        <!--Google Analytics-->
+
+        <!---->
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div>
+            <div class="test grid-1"><p>Test</p></div>
+            <div class="test grid-1"></div>
+            <div class="test grid-1"></div>
+            <div class="test grid-1"></div>
+            <div class="test grid-1"></div>
+            <div class="test grid-1"></div>
+        </div>
+        <div>
+            <div class="test grid-2"></div>
+            <div class="test grid-2"></div>
+            <div class="test grid-2"></div>
+        </div>
+        <div>
+            <div class="test grid-3"></div>
+            <div class="test grid-3"></div>
+        </div>
+        <div>
+            <div class="test grid-4"></div>
+            <div class="test grid-2"></div>
+        </div>
+        <div>
+            <div class="test grid-full"></div>
+        </div>
+        <div>
+            <div class="test grid-25"></div>
+            <div class="test grid-25"></div>
+            <div class="test grid-25"></div>
+            <div class="test grid-25"></div>
+        </div>
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="bower_components/jquery/dist/jquery.min.js"><\/script>')</script>
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
+<?php include 'includes/footer.php';?>
